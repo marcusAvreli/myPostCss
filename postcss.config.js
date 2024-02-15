373fa9424cf7569452a17590f6d0ca8254c185de
Mark Gavrilman <markgav19@gmail.com>
2024-02-15 19:59:19 +0200

    initial_commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: 'postcss-scss',
+  map: {
+    sourcesContent: true,
+    annotation: true
+  },
+  plugins: {
+    'postcss-node-sass': {
+    //  includePaths: ['node_modules'],
+      outputStyle: 'compressed'
+    },
+	
+    'autoprefixer': {}
+  }
+}
